a1465283115607ce35f0f68b2a4de48bc48d7d2f
Christophe Benz <christophe.benz@data.gouv.fr>
2015-04-21 17:45:38 +0200

    Remove jquery

--- webpack/copy_assets.sh
+++ webpack/copy_assets.sh
@@ -2,4 +2,3 @@
 
 test -d public/assets || mkdir -p public/assets
 test -d public/assets/bootstrap || cp -r node_modules/bootstrap/dist public/assets/bootstrap
-test -d public/assets/jquery || cp -r node_modules/jquery/dist public/assets/jquery
