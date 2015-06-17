4a83eabe42e58a652d12b18c998400e357c4283b
Christophe Benz <christophe.benz@data.gouv.fr>
2015-06-17 17:33:54 +0200

    Enhance formulas propTypes

--- src/app-prop-types.js
+++ src/app-prop-types.js
@@ -28,19 +28,14 @@ import ImmutablePropTypes from "react-immutable-proptypes";
 
 // Level 0 PropTypes
 
-var unit = PropTypes.oneOf([
-  "currency",
-  "day",
-  "hour",
-  "month",
-  "year",
-]);
-
-var formula = PropTypes.shape({
+var simpleFormula = PropTypes.shape({
+  comments: PropTypes.string,
+  doc: PropTypes.string,
   input_variables: PropTypes.arrayOf(PropTypes.string),
   line_number: PropTypes.number,
   module: PropTypes.string.isRequired,
   parameters: PropTypes.arrayOf(PropTypes.string),
+  source: PropTypes.string.isRequired,
 });
 
 var startStopValue = PropTypes.shape({
@@ -54,18 +49,52 @@ var startStopValue = PropTypes.shape({
   ]).isRequired,
 });
 
+var unit = PropTypes.oneOf([
+  "currency",
+  "day",
+  "hour",
+  "month",
+  "year",
+]);
+
 
 // Level 1 PropTypes
 
 var datedFormula = PropTypes.shape({
   dated_formulas: PropTypes.arrayOf(
     PropTypes.shape({
-      formula: formula.isRequired,
+      formula: simpleFormula.isRequired,
       start_instant: PropTypes.string,
       stop_instant: PropTypes.string,
     })
   ).isRequired,
 });
+
+var formula = (props, propName, componentName, location) => {
+  var formulaPropName = "formula";
+  if (formulaPropName in props) {
+    var formulaProps = props[formulaPropName];
+    var type = formulaProps["@type"];
+    var error;
+    switch(type) {
+      case "DatedFormula":
+        error = datedFormula(true, formulaProps, formulaPropName, componentName, location);
+        break;
+      case "EntityToPerson":
+      case "PersonToEntity":
+      case "SimpleFormula":
+        error = simpleFormula(true, formulaProps, formulaPropName, componentName, location);
+        break;
+      default:
+        return new Error(`Invalid "formula" property @type "${type}"`);
+    }
+    if (error) {
+      return new Error(`Invalid "formula" property of @type "${type}", \
+embedded in prop "${propName}" supplied to ${componentName}`);
+    }
+    return null;
+  }
+};
 
 var immutableChildren = ImmutablePropTypes.mapOf(immutableVariablesTree);
 
@@ -114,7 +143,7 @@ var scale = PropTypes.shape({
 });
 
 var variable = PropTypes.shape({
-  formula: PropTypes.oneOfType([formula, datedFormula]),
+  formula,
   label: PropTypes.string,
   line_number: PropTypes.number,
   module: PropTypes.string.isRequired,
