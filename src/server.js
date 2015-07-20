cd80aa56abeab892f3412b61dd82d09edfd32e89
Christophe Benz <christophe.benz@data.gouv.fr>
2015-07-20 17:41:03 +0200

    Add error emails

--- src/server.js
+++ src/server.js
@@ -1,10 +1,39 @@
 import compression from "compression";
+import emailjs from "emailjs";
 import express from "express";
 import favicon from "serve-favicon";
 import morgan from "morgan";
 import path from "path";
 
 import render from "./server/render";
+
+
+function sendErrorEmail(req, err) {
+  const server = emailjs.server.connect({
+    host: "localhost",
+  });
+  const fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+  server.send(
+    {
+      from: "webmaster+legislation-explorer@openfisca.fr",
+      subject: `Legislation Explorer Error: ${err.toString()}`,
+      text: `
+URL: ${fullUrl}
+
+${err.stack}
+
+${JSON.stringify(req.headers, null, 2)}`,
+      to: "christophe.benz@data.gouv.fr",
+    },
+    function(sendErr/*, message*/) {
+      if (sendErr) {
+        console.log("Error sending email:", sendErr);
+      } else {
+        console.log("Email sent");
+      }
+    }
+  );
+}
 
 
 const server = express();
@@ -27,11 +56,16 @@ if (server.get("env") === "development") {
 server.use(render);
 
 // Generic server errors (e.g. not caught by components)
-server.use((err, req, res/*, next*/) => {
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   console.log("Error on request %s %s", req.method, req.url);
-  console.log(err);
-  console.log(err.stack);
-  res.status(500).send("Something bad happened");
+  if (server.get("env") === "production") {
+    console.log("err", err);
+    console.log("err.stack", err.stack);
+    sendErrorEmail(req, err);
+    res.status(500).send("Something bad happened (server error). An email has been sent to the team.");
+  } else {
+    next(err);
+  }
 });
 
 server.set("port", process.env.PORT || 2030);
