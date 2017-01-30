6c3870ef64a45f6510878dd9d12e86ec4fa81a63
Christophe Benz <christophe.benz@data.gouv.fr>
2017-01-30 16:10:55 +0100

    Track Tunisia too

--- config/production.tunisia.js
+++ config/production.tunisia.js
@@ -5,7 +5,11 @@ const HOST = process.env.HOST || "api.openfisca.tn",
   apiBaseUrl = process.env.API_URL || `https://${HOST}`,
   gitHubProject = "openfisca/openfisca-tunisia",
   gitWebpageUrl = "https://github.com/openfisca/legislation-explorer",
-  piwikConfig = null,
+  piwikConfig = {
+    url: "https://stats.data.gouv.fr",
+    siteId: 4,
+    trackErrors: true
+  },
   useCommitReferenceFromApi = false,
   websiteUrl = "https://www.openfisca.fr/"
 
