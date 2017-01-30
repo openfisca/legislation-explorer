89ae2255d4d5859af8c61acfdb79301d7078b424
Christophe Benz <christophe.benz@data.gouv.fr>
2017-01-30 14:38:39 +0100

    Set piwikConfig to null for dev and tunisia envs

--- config/production.tunisia.js
+++ config/production.tunisia.js
@@ -5,6 +5,7 @@ const HOST = process.env.HOST || "api.openfisca.tn",
   apiBaseUrl = process.env.API_URL || `https://${HOST}`,
   gitHubProject = "openfisca/openfisca-tunisia",
   gitWebpageUrl = "https://github.com/openfisca/legislation-explorer",
+  piwikConfig = null,
   useCommitReferenceFromApi = false,
   websiteUrl = "https://www.openfisca.fr/"
 
@@ -13,6 +14,7 @@ export default {
   apiBaseUrl,
   gitHubProject,
   gitWebpageUrl,
+  piwikConfig,
   useCommitReferenceFromApi,
   websiteUrl
 }
