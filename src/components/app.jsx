ff0f5de91976ee9017b3a595cbae240db7948413
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-03-28 17:20:27 +0200

    Adapt parameters count

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -65,7 +65,7 @@ const App = React.createClass({
                 </div>
                 <div className="col-md-9">
                   <p id="baseline">
-                    {variables.length} variables et {parameters.length} paramètres
+                    {variables.length} variables et {Object.keys(parameters).length} paramètres
                     référencés pour modéliser le système socio-fiscal français.
                   </p>
                   <a href={config.websiteUrl}>En savoir plus</a>
