69411d478ccb1c438c3ade413e4339d72179c76e
Christophe Benz <christophe.benz@data.gouv.fr>
2017-04-19 12:17:07 +0200

    Add test scenario

---
+++ tests/6 - LookUpScaleScenario.js
@@ -0,0 +1,18 @@
+description: "Navigating to an scale should show its current and past values",
+
+steps: [
+  SearchBarComponent.searchFor(impotRevenuBaremeQuery),
+  AppComponent.goToFirstResult(),
+  {
+    "DetailsComponent.description": impotRevenuBaremeResultDescription,
+    "ScaleTableComponent.headerColumn1": dateHeader,
+    "ScaleTableComponent.headerColumn2": trancheHeader,
+    "ScaleTableComponent.headerColumn3": tauxMarginalHeader,
+    "ScaleTableComponent.firstBracketColumn1": ongoingDateRange,
+    "ScaleTableComponent.firstBracketColumn2": thresholdRange,
+    "ScaleTableComponent.firstBracketColumn3": rate,
+    "ScaleTableComponent.secondBracketColumn2": thresholdRange,
+    "ScaleTableComponent.secondBracketColumn3": rate,
+  },
+  DetailsComponent.goBack(),
+]
---
+++ tests/ScaleTableFixture.js
@@ -0,0 +1,6 @@
+dateHeader = "Date"
+trancheHeader = "Tranche"
+tauxMarginalHeader = "Taux marginal"
+ongoingDateRange = /partir du \d{2}\/\d{2}\/\d{4}/
+thresholdRange = /Entre \d+ et \d+/
+rate = /\d+(.\d+)? %/
