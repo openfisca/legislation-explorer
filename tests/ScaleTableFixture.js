94686031e43ecd1165085c587510b7a538029aaa
Christophe Benz <christophe.benz@data.gouv.fr>
2017-04-19 18:52:27 +0200

    Update date regexes to match Android date rendering

--- tests/ScaleTableFixture.js
+++ tests/ScaleTableFixture.js
@@ -1,6 +1,6 @@
 dateHeader = "Date"
 trancheHeader = "Tranche"
 tauxMarginalHeader = "Taux marginal"
-ongoingDateRange = /partir du \d{2}\/\d{2}\/\d{4}/
+ongoingDateRange = /partir du \d{1,2}\/\d{1,2}\/\d{4}/
 thresholdRange = /Entre \d+ et \d+/
 rate = /\d+(.\d+)? %/
