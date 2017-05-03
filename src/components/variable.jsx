43e4307ffefb0c6e53667904e4d82af9501c7688
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-03 09:32:09 +0200

    Display variable metadata

--- src/components/variable.jsx
+++ src/components/variable.jsx
@@ -11,32 +11,6 @@ import GitHubLink from "./github-link"
 import List from "./list"
 
 
-
-function isConsumerVariable(variable, name) {
-  // Does `variable` consume `name` as an input variable?
-  function isConsumerFormula(formula) {
-    return formula.input_variables && formula.input_variables.includes(name)
-  }
-  const {formula} = variable
-  if (!formula) {
-    return false
-  }
-  switch (formula["@type"]) {
-    case "SimpleFormula":
-      return isConsumerFormula(formula)
-    case "DatedFormula":
-      return formula.dated_formulas.some(dated_formula => isConsumerFormula(dated_formula.formula))
-    case "EntityToPerson":
-    case "PersonToEntity":
-      // Ignore those variable types since they have an implicit formula, not using any variable of the legislation.
-      return false
-    default:
-      console.error("Unexpected formula type:", formula["@type"])
-      return false
-  }
-}
-
-
 const Variable = React.createClass({
   propTypes: {
     countryPackageName: PropTypes.string.isRequired,
@@ -50,268 +24,73 @@ const Variable = React.createClass({
   },
   render() {
     const {variable} = this.props
-    const {formula, description, source_code} = variable
-    const labelMessage = description || `${variable.name} (aucun label)`
     return (
-      <DocumentTitle title={`${labelMessage} - Explorateur de la législation`}>
+      <DocumentTitle title={`${variable.id} - Explorateur de la législation`}>
         <div>
-          <div className="page-header">
-            <h1>{labelMessage}</h1>
+          <header className="page-header">
+            <h1>{variable.id}</h1>
+            <p>{variable.description}</p>
+          </header>
+          {this.renderVariableMetadata(variable)}
+          <div>
+            <ExternalLink href={variable.source}>
+              Voir le code source de cette variable
+            </ExternalLink>
           </div>
-          {this.renderDefinitionsLists(variable)}
-          {formula && <hr />}
-          {
-            formula && (
-              formula["@type"] === "DatedFormula"
-                ? this.renderDatedFormula(formula)
-                : this.renderSimpleFormula(formula)
-            )
-          }
-          <hr />
-          <h4>Code source</h4>
-          <pre>{source_code}</pre>
-          {this.renderSourceCodeLink(variable)}
-          <hr />
-          <ExternalLink
-            href={`${config.apiBaseUrl}/api/1/variables?name=${variable.name}`}
-            title="Voir la donnée brute au format JSON"
-          >
-            Export JSON
-          </ExternalLink>
+          <div>
+            <ExternalLink href={`${config.apiBaseUrl}/variable/${variable.id}`}>
+              Voir la donnée brute au format JSON
+            </ExternalLink>
+          </div>
         </div>
       </DocumentTitle>
     )
   },
-  renderConsumerVariables(variable) {
-    const {variables} = this.props
-    const consumerVariables = variables.filter(variable2 => isConsumerVariable(variable2, variable.name))
-    return [
-      <dt key="dt">Formules utilisant cette variable</dt>,
-      <dd key="dd">
-        {
-          consumerVariables && consumerVariables.length ? (
-            <List items={sortBy(prop("name"), consumerVariables)} type="inline">
-              {variable2 => <Link title={variable2.label} to={`/${variable2.name}`}>{variable2.name}</Link>}
-            </List>
-          ) : (
-              <span className="label label-warning">Aucune</span>
-            )
-        }
-      </dd>,
-    ]
-  },
-  renderDatedFormula(formula) {
-    return formula.dated_formulas.map((datedFormula, idx) => (
-      <div key={idx}>
-        <h4 style={{ display: "inline-block" }}>
-          {this.renderDatedFormulaHeading(datedFormula)}
-        </h4>
-        {this.renderFormula(datedFormula.formula)}
-        <hr />
-      </div>
-    ))
-  },
-  renderDatedFormulaHeading(formula) {
-    if (formula.start_instant && formula.stop_instant) {
-      return (
-        <span>
-          Formule de calcul du
-          {" "}
-          <FormattedDate value={formula.start_instant} />
-          {" "}
-          au
-          {" "}
-          <FormattedDate value={formula.stop_instant} />
-        </span>
-      )
+  renderVariableMetadata(variable) {
+    const entityMessage = {
+      famille: "une famille",
+      foyer_fiscal: "un foyer fiscal",
+      individu: "un individu",
+      menage: "un ménage",
     }
-    if (formula.start_instant) {
-      return (
-        <span>
-          Formule de calcul depuis le
-          {" "}
-          <FormattedDate value={formula.start_instant} />
-        </span>
-      )
+    const definitionPeriodMessage = {
+      YEAR: "de l'année",
+      MONTH: "du mois",
+      ETERNITY: "de l'éternité (cette variable ne peut pas évoluer avec le temps",
     }
-    if (formula.stop_instant) {
-      return (
-        <span>
-          Formule de calcul jusqu'au
-          {" "}
-          <FormattedDate value={formula.stop_instant} />
-        </span>
-      )
-    }
-  },
-  renderFormula(formula) {
-    const {parameters} = this.props
-    const inputVariableNames = formula.input_variables
-    const formulaParameterNames = formula.parameters
     return (
       <div>
+        <dl>
+          <dt>Cette variable est définie pour</dt>
+          <dd>{entityMessage[variable.entity]}.</dd>
+          <dt>Elle se calcule à l'échelle</dt>
+          <dd>{definitionPeriodMessage[variable.definitionPeriod]}</dd>
+          <dt>Elle est de type</dt>
+          <dd>{variable.valueType}</dd>
+          <dt>Sa valeur par défault est</dt>
+          <dd>{String(variable.defaultValue)}</dd>
         {
-          (inputVariableNames || formulaParameterNames) && (
-            <dl>
-              {inputVariableNames && <dt>Variables utilisées</dt>}
-              {
-                inputVariableNames && (
-                  <dd>
-                    <List items={sort(substract, inputVariableNames)} type="inline">
-                      {name => <Link to={`/${name}`}>{name}</Link>}
-                    </List>
-                  </dd>
-                )
-              }
-              {formulaParameterNames && <dt>Paramètres utilisés</dt>}
-              {
-                formulaParameterNames && (
-                  <dd>
-                    <List items={sort(substract, formulaParameterNames)} type="inline">
-                      {
-                        parameterName => {
-                          const parameter = parameters[parameterName]
-                          if (parameter) {
-                            return (
-                              <span>
-                                <Link title={parameter.description} to={`/${parameterName}`}>
-                                  {parameterName}
-                                </Link>
-                              </span>
-                            )
-                          } else {
-                            return (
-                              <span>
-                                {parameterName}
-                                {" "}
-                                <span className="label label-warning">inexistant</span>
-                              </span>
-                            )
-                          }
-                        }
-                      }
-                    </List>
-                  </dd>
-                )
-              }
-            </dl>
+          variable.references && (
+            <span>
+              <dt>Références :</dt>
+              <dd><ul>
+                {
+                  variable.references.map((reference, idx) =>
+                    <li key={idx}>
+                      <ExternalLink href={reference}>
+                        {reference}
+                      </ExternalLink>
+                    </li>
+                  )
+                }
+              </ul></dd>
+            </span>
           )
         }
-      </div>
-    )
-  },
-  renderSimpleFormula(formula) {
-    return (
-      <div>
-        <h4 style={{ display: "inline-block" }}>Formule de calcul</h4>
-        {this.renderFormula(formula)}
-      </div>
-    )
-  },
-  renderDefinitionsLists(variable) {
-    // TODO Do not hardcode entities.
-    const entityMessageByNamePlural = {
-      famille: "de la famille",
-      foyer_fiscal: "du foyer fiscal",
-      individu: "de l'individu",
-      menage: "du ménage",
-    }
-    const type = variable["@type"]
-    return (
-      <div>
-        <dl>
-          <dt>Se calcule à l'échelle</dt>
-          <dd>{entityMessageByNamePlural[variable.entity]}</dd>
-          {
-            variable.cerfa_field && [
-              <dt key="dt">Cases CERFA</dt>,
-              <dd key="dd">
-                {
-                  typeof variable.cerfa_field === "string" ?
-                    variable.cerfa_field :
-                    Object.values(variable.cerfa_field).join(", ")
-                }
-              </dd>,
-            ]
-          }
-          {
-            variable.start && [
-              <dt key="dt">Entre en vigueur à partir du</dt>,
-              <dd key="dd">
-                <FormattedDate value={variable.start} />
-              </dd>,
-            ]
-          }
-          {
-            variable.end && [
-              <dt key="dt">N'est plus en vigueur à partir du</dt>,
-              <dd key="dd">
-                <FormattedDate value={variable.end} />
-              </dd>,
-            ]
-          }
-          {
-            variable.url && [
-              <dt key="dt">URL externe</dt>,
-              <dd key="dd">
-                <ExternalLink href={variable.url}>
-                  {variable.url}
-                </ExternalLink>
-              </dd>,
-            ]
-          }
-        </dl>
-        <hr />
-        <h4>Données spécifiques à OpenFisca</h4>
-        <dl>
-          <dt>Nom de la variable</dt>
-          <dd>{variable.name}</dd>
-          <dt>Type</dt>
-          <dd>
-            <code>{type}</code>
-            {variable.val_type && ` (${variable.val_type})`}
-          </dd>
-          {type === "Enumeration" && <dt>Libellés</dt>}
-          {
-            type === "Enumeration" && (
-              <dd>
-                <List items={Object.entries(variable.labels)} type="unstyled">
-                  {entry => `${entry[0]} = ${entry[1]}`}
-                </List>
-              </dd>
-            )
-          }
-          <dt>Valeur par défaut</dt>
-          <dd>
-            <samp>
-              {
-                type === "Boolean" ?
-                  JSON.stringify(variable.default) :
-                  variable.default
-              }
-            </samp>
-          </dd>
-          {this.renderConsumerVariables(variable)}
         </dl>
       </div>
     )
-  },
-  renderSourceCodeLink(variable) {
-    const {countryPackageName, countryPackageVersion} = this.props
-    let sourceCodeText = variable.source_file_path.split('/').slice(-1)
-    if (variable.start_line_number) {
-      sourceCodeText += ` ligne ${variable.start_line_number}`
-    }
-    return (
-      <GitHubLink
-        blobUrlPath={countryPackageName + '/' + variable.source_file_path}
-        commitReference={countryPackageVersion}
-        lineNumber={variable.start_line_number}
-        style={{ marginLeft: "1em" }}
-        text={sourceCodeText}
-      />
-    )
-  },
+  }
 })
 
 
