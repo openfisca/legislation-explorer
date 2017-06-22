f9930ab6995add61983db0751fc4110db134e4a3
sandcha <sandcha@users.noreply.github.com>
2017-06-22 04:30:15 +0200

    Load detected react-intl locale data

--- src/server/lang.js
+++ src/server/lang.js
@@ -2,23 +2,21 @@ import path from "path"
 import {readdir} from "fs"
 
 import {addLocaleData} from "react-intl"
-import fr from "react-intl/locale-data/fr"
-import en from "react-intl/locale-data/en"
 
 const DEFAULT_LANGUAGE = 'fr'
 
 export function loadTranslations(langDir) {
-  addLocaleData(...fr)
-  addLocaleData(...en)
-
   var messages = {}
   readdir(langDir, (err, files) => {
     if(err) {
       console.log("Unable to load translation files.", err)
     }
 
+    var possibleLocale
     files.forEach(file => {
-      messages[path.basename(file, '.json')] = require(path.resolve(langDir, file))
+      possibleLocale = path.basename(file, '.json')
+      addLocaleData(require(`react-intl/locale-data/${possibleLocale}`))
+      messages[possibleLocale] = require(path.resolve(langDir, file))
     })
   })
 
