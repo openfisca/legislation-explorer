25be606aaf9841ee76a5eb5e7acfbd213d4ad02b
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-06-30 13:30:35 +0200

    Test single-word research

--- tests/unit/search.js
+++ tests/unit/search.js
@@ -1,7 +1,17 @@
-const search = require("../../src/search")
+import {addNormalizedDescription, findParametersAndVariables} from '../../src/search'
+import {equals} from "ramda"
+import assert from "assert"
+
+const parameters = addNormalizedDescription({
+    'aah': {description: 'Allocation adulte handicapé'},
+    'cotisation': {description: 'cotisation payée sur le salaire'},
+    'salaire_de_base': {description: 'Salaire brut'},
+});
 
 describe('Search', function() {
-    it('should not crash', function() {
-        search.findParametersAndVariables({}, {}, "");
+    it('should filter variables that don’t contain the query', function() {
+        const results = findParametersAndVariables(parameters, {}, 'salaire')
+            .map(item => item.name);
+        assert(equals(results, ['salaire_de_base', 'cotisation']))
     });
 });
