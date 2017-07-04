b217367db468e37bc01b980657f77c191672f3c2
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-07-04 18:25:33 +0200

    Add unit tests

--- tests/unit/formulas.js
+++ tests/unit/formulas.js
@@ -8,6 +8,8 @@ import should from "should"
 const variables = {
     rsa: {},
     rsa_base_ressource: {},
+    rsa_fictif: {},
+    rsa_eligible: {},
 }
 const variable = {id: 'rsa'}
 const component = shallow(<Variable variable={variable} variables={variables} />).instance()
@@ -19,8 +21,35 @@ describe('Link rendering in variables', function() {
     it('should add links to a variables with double quotes', function() {
         const formula = 'simulation.calculate("rsa_base_ressource")'
         const output = renderLinkedFormulaVariables(formula)
-        output.find(Link).should.have.length(1);
-        const link = output.find(Link).first();
-        link.props().to.should.equal("rsa_base_ressource");
+        const links = output.find(Link)
+        links.should.have.length(1)
+        links.get(0).props.to.should.equal('rsa_base_ressource')
+    });
+
+    it('should add links to a variables with simple quotes', function() {
+        const formula = 'simulation.calculate(\'rsa_base_ressource\')'
+        const output = renderLinkedFormulaVariables(formula)
+        const links = output.find(Link)
+        links.should.have.length(1)
+        links.get(0).props.to.should.equal('rsa_base_ressource')
+    });
+
+    it('should handle simple quotes and double quotes mixed', function() {
+        const formula = 'simulation.calculate(\'rsa_base_ressource\') simulation.calculate("rsa_fictif") simulation.calculate(\'rsa_eligible\')'
+        const output = renderLinkedFormulaVariables(formula)
+        const links = output.find(Link)
+        links.should.have.length(3)
+        links.get(0).props.to.should.equal('rsa_base_ressource');
+        links.get(1).props.to.should.equal('rsa_fictif');
+        links.get(2).props.to.should.equal('rsa_eligible');
+    });
+
+    it('should be robust to quotes in comments', function() {
+        const formula = ' # This a a comment with a quote \' simulation.calculate(\'rsa_base_ressource\') simulation.calculate(\'rsa_eligible\')'
+        const output = renderLinkedFormulaVariables(formula)
+        const links = output.find(Link)
+        links.should.have.length(2)
+        links.get(0).props.to.should.equal('rsa_base_ressource');
+        links.get(1).props.to.should.equal('rsa_eligible');
     });
 });
