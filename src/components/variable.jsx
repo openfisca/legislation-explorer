fbaa23a7a2ef0edc9fee823651541b35e71a8a5e
Anna-Livia <annalivia.cardin@gmail.com>
2017-07-11 07:35:24 +0200

    fixup! Update the variable's metadata description

--- src/components/variable.jsx
+++ src/components/variable.jsx
@@ -1,7 +1,7 @@
 import DocumentTitle from "react-document-title"
 import {FormattedMessage, FormattedDate} from "react-intl"
 import React, { PropTypes } from "react"
-import {Link} from "react-router"
+import { Link } from "react-router"
 import { keys } from "ramda"
 
 import config from "../config"
@@ -22,24 +22,24 @@ const Variable = React.createClass({
     return new Date().toJSON().slice(0, 10)
   },
   render() {
-    const {variable} = this.props
+    const { variable } = this.props
     return (
-      <DocumentTitle title={`${variable.id} - Explorateur de la législation`}>
+      <DocumentTitle title = { `${ variable.id } - Explorateur de la législation` } >
         <div>
-          <header className="page-header">
-            <h1><code>{variable.id}</code></h1>
-            <p className="descriptionForVariables">{variable.description}</p>
+          <header className = "page-header">
+            <h1><code> { variable.id } </code></h1>
+            <p className = "descriptionForVariables">{variable.description}</p>
           </header>
           {this.renderVariableMetadata(variable)}
           <div>
-            <ExternalLink href={variable.source}>
-              <FormattedMessage id='editThisVariable'/>
+            <ExternalLink href = { variable.source } target = "_blank">
+              <FormattedMessage id = "editThisVariable"/>
             </ExternalLink>
           </div>
-          {keys(variable.formulas).length != 0 && this.renderFormulas(variable.formulas)}
+          { keys(variable.formulas).length != 0 && this.renderFormulas(variable.formulas) }
           <div>
-            <ExternalLink href={`${config.apiBaseUrl}/variable/${variable.id}`}>
-              <FormattedMessage id='thisVariableJson'/>
+            <ExternalLink href = { `${ config.apiBaseUrl } /variable/${ variable.id }` } target = "_blank" >
+              <FormattedMessage id = "thisVariableJson"/>
             </ExternalLink>
           </div>
         </div>
@@ -54,101 +54,97 @@ const Variable = React.createClass({
       menage: "ménage",
     }
     const definitionPeriodMessage = {
-      YEAR: <FormattedMessage id='aYear'/>,
-      MONTH: <FormattedMessage id='aMonth'/>,
-      ETERNITY: <FormattedMessage id='forEternity'/>
+      YEAR: <FormattedMessage id = "aYear"/>,
+      MONTH: <FormattedMessage id = "aMonth"/>,
+      ETERNITY: <FormattedMessage id = "forEternity"/>
     }
     const valueTypeMessage = {
-      Int: <FormattedMessage id='anInteger'/>,
-      Float: <FormattedMessage id='aFloat'/>,
-      Date: <FormattedMessage id='aDate'/>,
+      Int: <FormattedMessage id = "anInteger"/>,
+      Float: <FormattedMessage id = "aFloat"/>,
+      Date: <FormattedMessage id = "aDate"/>,
     }
-    function formatDefaultValue(variable){
-    if (variable.valueType == "Date"){
-      return <FormattedDate value={variable.defaultValue} year='numeric' month='2-digit' day='2-digit'/>
-    }else if(variable.valueType == "Float"){
-      if (String(variable.defaultValue).indexOf('.') != -1){
-        return String("variable.defaultValue").indexOf('.')
-      }else{
-        return String(variable.defaultValue) + '.0';
-      }
-    }else{
+    function formatDefaultValue(variable) {
+    if (variable.valueType == "Date") {
+      return <FormattedDate value = { variable.defaultValue } year = "numeric" month = "2-digit" day = "2-digit" />
+    } else if (variable.valueType == "Float") {
+        return variable.defaultValue.toFixed(1);
+    } else {
       return String(variable.defaultValue)
     }
   }
     return (
       <div>
         <p>
-            <FormattedMessage id='entityParagraph'
-              values={{
+            <FormattedMessage id = "entityParagraph"
+              values = {{
                 variableLink: 
-                  <a href="href='https://doc.openfisca.fr/variables.html'">
-                    <FormattedMessage id='variableText'/>
+                  <a href = "https://doc.openfisca.fr/variables.html" target = "_blank">
+                    <FormattedMessage id = "variableText"/>
                   </a>,
                 entityLink:
-                  <a href="https://doc.openfisca.fr/periodsinstants.html">
-                    <FormattedMessage id='entityText'/>
+                  <a href = "https://doc.openfisca.fr/person,_entities,_role.html" target = "_blank">
+                    <FormattedMessage id = "entityText"/>
                   </a>
-                }}/>
-            <span className="variableMetadataHighlight">
-              {entityMessage[variable.entity]}
+                }} />
+            <span className = "variableMetadataHighlight">
+              { entityMessage[variable.entity] }
             </span>
           .
         </p>
         <p>
-          <FormattedMessage id='definitionPeriodParagraph' 
-            values={{definitionPeriodLink: 
-              <a href="https://doc.openfisca.fr/periodsinstants.html">
-              <FormattedMessage id='definitionPeriodText'/></a>}}
+          <FormattedMessage id = "definitionPeriodParagraph"
+            values = {{ definitionPeriodLink:
+              <a href = "https://doc.openfisca.fr/coding-the-legislation/35_periods.html#periods-for-variable" target = "_blank">
+              <FormattedMessage id = "definitionPeriodText"/></a> }}
             />
-          <span className="variableMetadataHighlight">
-            {definitionPeriodMessage[variable.definitionPeriod]}
+          <span className = "variableMetadataHighlight">
+            { definitionPeriodMessage[variable.definitionPeriod] }
           </span>
           .
         </p>
         <p>
-          <FormattedMessage id='valueTypeParagraph'/> 
-          <span className="variableMetadataHighlight">
+          <FormattedMessage id = "valueTypeParagraph"/>
+          <span className = "variableMetadataHighlight">
             {variable.valueType in valueTypeMessage ? (
-              <span> {valueTypeMessage[variable.valueType]}.</span>
+              <span> { valueTypeMessage[variable.valueType] }.</span>
             ) : (
-              <span> {variable.valueType}.</span>
+              <span> { variable.valueType }.</span>
             )}
           </span>
         </p>
         <p>
-          <FormattedMessage id='defaultValueParagraph'
-            values={{defaultValueLink:
-              <a href="https://doc.openfisca.fr/coding-the-legislation/20_input_variables.html">
-              <FormattedMessage id='defaultValueText'/>
-              </a>}
+          <FormattedMessage id = "defaultValueParagraph"
+            values = {{ defaultValueLink:
+              <a href = "https://doc.openfisca.fr/coding-the-legislation/20_input_variables.html" target = "_blank">
+              <FormattedMessage id = "defaultValueText"/>
+              </a> }
             }
           />
-          <span className="variableMetadataHighlight">
+          <span className = "variableMetadataHighlight">
             {formatDefaultValue(variable)}
           </span>
           .
         </p> 
           { variable.references && 
-            (<span><FormattedMessage id='referencesText'/>&nbsp;:</span>)
+            (<span><FormattedMessage id = "referencesText"/>&nbsp;:</span>)
           }
           { variable.references && 
             (<ul>
               {
                 variable.references.map((reference, idx) =>
-                  <li key={idx}>
-                    <a href={reference}>{reference}</a>
+                  <li key = { idx } >
+                    <a href = { reference } > { reference } target = "_blank"</a>
                   </li>
                 )}
            </ul>)
           }
           {keys(variable.formulas).length == 0 &&
             <p>
-              <FormattedMessage id='noFormulaParagraph'
-                values={{
+              <FormattedMessage id = "noFormulaParagraph"
+                values = {{
                   formulaNotComputable:
-                  <span className="variableMetadataHighlight">
-                    <FormattedMessage id='formulaNotComputableText'/>
+                  <span className = "variableMetadataHighlight">
+                    <FormattedMessage id = "formulaNotComputableText"/>
                   </span>
                 }}
               />
@@ -164,29 +160,29 @@ const Variable = React.createClass({
       <div>
         <h2>
         {severalFormulas ? (
-              <FormattedMessage id='formulasTitle'/>
+              <FormattedMessage id = "formulasTitle"/>
             ) : (
-              <FormattedMessage id='formulaTitle'/>
+              <FormattedMessage id = "formulaTitle"/>
             )}
         </h2>
-        {startDates.map(
+        { startDates.map(
           (date, dateIndex) => {
-            const startDate = (date != '0001-01-01') && date
+            const startDate = (date != "0001-01-01") && date
             const stopDate = startDates[dateIndex - 1] && getDayBefore(startDates[dateIndex - 1])
             return formulas[date] && (
-              <div key={date}>
+              <div key = { date } >
                 {startDate && (! stopDate) &&
-                  <h3>À partir du <FormattedDate value={startDate} />&nbsp;:</h3>
+                  <h3>À partir du <FormattedDate value = { startDate } />&nbsp;:</h3>
                 }
                 {stopDate && (! startDate) &&
-                  <h3>Jusqu'au <FormattedDate value={stopDate} />&nbsp;:</h3>
+                  <h3>Jusqu'au <FormattedDate value = { stopDate } />&nbsp;:</h3>
                 }
                 {startDate && stopDate &&
-                  <h3>Du <FormattedDate value={startDate}/> au <FormattedDate value={stopDate} />&nbsp;:</h3>
+                  <h3>Du <FormattedDate value = { startDate } /> au <FormattedDate value = { stopDate } />&nbsp;:</h3>
                 }
-                <Highlight className='python'>{this.renderLinkedFormulaVariables(formulas[date].content)}</Highlight>
+                <Highlight className = "python"> { this.renderLinkedFormulaVariables(formulas[date].content) } </Highlight>
               <p>
-                 <a href={formulas[date].source}><FormattedMessage id='editThisFormula'/></a>
+                 <a href = { formulas[date].source } ><FormattedMessage id = "editThisFormula" target = "_blank"/></a>
               </p>
               </div>
             )
@@ -197,8 +193,8 @@ const Variable = React.createClass({
     )
   },
   link(variable) {
-    return <Link key={variable + Math.random()} to={variable}
-      data-toggle="popover" title={this.props.variables[variable].description}>{variable}</Link>
+    return <Link key = { variable + Math.random() } to = { variable }
+      data-toggle="popover" title = { this.props.variables[variable].description } > { variable } </Link>
   },
   isVariable(substring) {
     // Ignore every text that isn't a single word like a variable must be:
