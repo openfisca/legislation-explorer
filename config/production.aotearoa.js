2f26922cac915e393bea8bef278969b2cef477ff
Matti Schneider <labplus@matti.consulting>
2018-05-14 15:48:13 +1200

    Add Aotearoa New Zealand production config example

---
+++ config/production.aotearoa.js
@@ -0,0 +1,15 @@
+export default {
+  apiBaseUrl: process.env.API_URL || 'https://openfisca-aotearoa.herokuapp.com',
+  gitHubProject: 'ServiceInnovationLab/openfisca-aotearoa',
+  gitWebpageUrl: 'https://github.com/ServiceInnovationLab/openfisca-aotearoa',
+  useCommitReferenceFromApi: false,
+  websiteUrl: 'https://openfisca.org/',
+  ui: {
+    en: {
+      countryName: 'Aotearoa New Zealand',
+    },
+    fr: {
+      countryName: 'Nouvelle-Zélande',
+    }
+  }
+}
