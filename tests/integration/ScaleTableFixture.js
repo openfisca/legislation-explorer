86468bb8bd45f0c4b26d6c74cc2da269a4ad2f5b
Matti Schneider <labplus@matti.consulting>
2018-05-20 17:27:09 +1200

    Update scale integration tests

--- tests/integration/ScaleTableFixture.js
+++ tests/integration/ScaleTableFixture.js
@@ -1,6 +1,3 @@
-dateHeader = "Date"
-trancheHeader = "Tranche"
-tauxMarginalHeader = "Taux marginal"
-ongoingDateRange = /partir du \d{1,2}\/\d{1,2}\/\d{4}/
-thresholdRange = /Entre \d+ et \d+/
-rate = /\d+(.\d+)? %/
+ongoingDateRange = /\d{2}\/\d{2}\/\d{4}/
+thresholdRange = /\d+.+\d+/
+rate = /\d+(.\d+)? ?%/
