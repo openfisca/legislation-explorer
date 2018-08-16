f3009159bf4da804380efd217bbfd27e914c00b2
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-08-15 23:40:27 -0400

    Don't add / if it's already there

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -20,7 +20,7 @@ module.exports = {
   output: {
     path: assetsPath,
     filename: '[name]-[hash].js',
-    publicPath: config.pathname + '/',
+    publicPath: config.pathname.endsWith('/') ? config.pathname : `${config.pathname}/`
   },
   target: 'web',
   // yaml-js has a reference to `fs`, this is a workaround
