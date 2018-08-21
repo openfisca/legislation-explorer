85641e0db06224488dab96f69eed31f0b2a0d4ba
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-08-21 14:14:40 -0400

    Use a more recent Android for integration tests

--- tests/integration/config-ci.js
+++ tests/integration/config-ci.js
@@ -1,16 +1,26 @@
 // this file is for use in CircleCI continuous integration environment
 
-var browserName = ['chrome', 'firefox', 'internet explorer', 'android'][process.env.CIRCLE_NODE_INDEX];
+var capabilities = [
+  {browserName: 'chrome'},
+  {browserName: 'firefox'},
+  {browserName: 'internet explorer'},
+  {
+    deviceName: 'Samsung Galaxy S6 GoogleAPI Emulator',
+    browserName: 'Chrome',
+    platformName: 'Android',
+    platformVersion: '7.0'
+  }
+  ][process.env.CIRCLE_NODE_INDEX];
 
 module.exports = {
   seleniumServerURL: {
     hostname: 'ondemand.saucelabs.com',
     port: 80
   },
-  driverCapabilities: {
-    'tunnel-identifier': 'circle-' + process.env.CIRCLE_BUILD_NUM + '-' + process.env.CIRCLE_NODE_INDEX,
-    browserName: browserName
-  },
+  driverCapabilities: Object.assign({},
+    capabilities,
+    {'tunnel-identifier': 'circle-' + process.env.CIRCLE_BUILD_NUM + '-' + process.env.CIRCLE_NODE_INDEX}
+  ),
   tags: [ 'circle-ci', '#' + process.env.CIRCLE_BUILD_NUM ],
   views: [ 'Verbose', 'SauceLabs' ],
   quit: 'always', // avoid wasting 90 seconds on SauceLabs
